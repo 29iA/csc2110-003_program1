33464250364e91f8d6e8b1c9e3e4738a82eb0ce5
29iA <29congtuanp@gmail.com>
2017-02-16 13:41:19 -0600

    building framework...

--- SortedListArrayDriver.cpp
+++ SortedListArrayDriver.cpp
@@ -11,21 +11,41 @@ using namespace std;
 
 void deleteCDs(ListArray<CD>* list)
 {
-   ListArrayIterator<CD>* iter = list->iterator();
+	ListArrayIterator<CD>* iter = list->iterator();
 
-   while(iter->hasNext())
-   {
-      CD* cd = iter->next();
-      delete cd;
-   }
-   delete iter;
+	while(iter->hasNext())
+	{
+		CD* cd = iter->next();
+		delete cd;
+	}
+	delete iter;
 }
 
 int main()
 {
-   ListArray<CD>* cds = CD::readCDs("cds.txt");
-
-   //thoroughly test your sorted list here
+	//include things from instruction
+	ListArray<CD>* cds = CD::readCDs("cds.txt");
+	SortedListArray* sl = new SortedListArray(&CD::compare_items, &CD::compare_keys);
+	
+	//thoroughly test your sorted list here
+	
+	//check 1: check size of newly made list
+	cout << "Current size: " << sl->size() << endl;
+	
+	//check 2: insert cds into the array
+	//TODO
+	
+	//check 3: check size of new array
+	cout << "Current size: " << sl->size() << endl;
+	
+	if (sl->isEmpty())
+		cout << "List is empty." << endl;
+	else
+		cout << "List is not empty, geting data..." << endl;
+	
+	//check 4: Display the current array
+	
+	//check+
 	//TODO
 
 
@@ -35,11 +55,8 @@ int main()
 
 
 
+	deleteCDs(cds);
+	delete cds;
 
-
-
-   deleteCDs(cds);
-   delete cds;
-
-   return 0;
+	return 0;
 }
